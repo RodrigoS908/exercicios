ecb6a4d94ab79892ae92ab374d1783ca33b4efba
Rodrigo Silvério dos Santos <silveriorodrigo9@gmail.com>
2023-04-18 11:40:20 -0300

    exercicio3

---
+++ Animais/src/animais/Gato.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package animais;
+
+/**
+ *
+ * @author 82318900
+ */
+public class Gato extends Mamifero{
+    public Gato(){
+        patas=4;
+        nome="Gato";
+        System.out.println("Construindo"+nome+"de"+patas+"patas");
+    }
+}
